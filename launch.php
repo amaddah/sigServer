c2ba7c4fe5c227ae93af521c5b2b44d627fbce58
amaddah <amine.maddah@insa-cvl.fr>
2016-05-13 18:08:12 +0200

    ajout ip en argument

--- launch.php
+++ launch.php
@@ -15,10 +15,13 @@ define("PRIVILEGE_ERROR", "Privilege Error");
 define("DATA_ERROR", "Protocol Error");
 define("TOKEN_ERROR", "Token Error");
 
+if(!isset($argv[1]))
+    die("Usage : php launch.php < IPDuServer >\n");
+
 $bdd = new BDD();
 $log = new Log(__DIR__ . "/logs/test.txt");
 
-$serv = new Server("udp", "192.168.0.37", 5000, $bdd, $log); //  Constructeur de serveur prend 3 parametres : le protocol, l'ip et enfin le port
+$serv = new Server("udp", $argv[1], 5000, $bdd, $log); //  Constructeur de serveur prend 3 parametres : le protocol, l'ip et enfin le port
 
 $serv->runServer(); // Ecoute en UDP sur le port 5000 sur la machine locale
 $serv->closeSocket(); // Ferme la socket UDP localhost:5000
